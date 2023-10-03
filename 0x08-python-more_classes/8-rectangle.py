f02e02152caa7956239ee529ba5f7b6347322d11
Senyo197 <Senyo197@gmail.com>
2023-10-03 05:36:42 +0000

    correction 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """A class to represent a Rectangle
 Attributes:
-    __width (int): The width of the rectangle
-    __height (int): The height of the rectangle
+    number_of_instances (int): The number of Rectangle instances.
+    print_symbol (any): The symbol used for string representation.
 """
 
 
