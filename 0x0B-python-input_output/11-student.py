928729a45c271750723d57fd2a22ca1770bf195c
Senyo197 <Senyo197@gmail.com>
2023-10-11 05:39:12 +0000

    set attibute value

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""A class representation of student"""
+
+
+class Student:
+    """Initialization of the new student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization of the new student class
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self attrs=None):
+        """json dictionary representation of student
+        Returns:
+            dictionary representation
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            filtered_dict = []
+            for attr in attrs:
+                if hasattr(self, attr):
+                    filtered_dict[attr] = getattr(self, attr)
+                return filtered_dict
+
+    def reload_from_json(self, json):
+        """Set the attribute for the json data"""
+        for key, value in json_data.items():
+            setattr(self, key, value)
