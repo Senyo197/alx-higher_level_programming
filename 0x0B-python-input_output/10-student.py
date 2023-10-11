f10f7ff30bba3bd341f69f8516c8bf22d69cc5f9
Senyo197 <Senyo197@gmail.com>
2023-10-11 05:28:19 +0000

    dictionary representation and filteration

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""A class representation of student"""
+
+
+class Student:
+    """Initialization of the new student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization of the new student class
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self attrs=None):
+        """json dictionary representation of student
+        Returns:
+            dictionary representation
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            filtered_dict = []
+            for attr in attrs:
+                if hasattr(self, attr):
+                    filtered_dict[attr] = getattr(self, attr)
+                return filtered_dict
