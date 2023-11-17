385d0b986544d40b08c540111114df69452a0765
Senyo197 <Senyo197@gmail.com>
2023-11-17 05:54:09 +0000

    corrections

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,8 +3,4 @@ CREATE IF NOT EXISTS DATABASE hbtn_0d_usa;
 -- Switch to the hbtn_0d_usa database
 USE hbtn_0d_usa;
 -- Create the states table if it doesn't exist
-CREATE TABLE IF NOT EXISTS states (
-    id INT UNIQUE NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY(id)
-);
+CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT UNIQUE NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
