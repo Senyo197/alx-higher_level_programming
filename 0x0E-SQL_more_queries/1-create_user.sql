8ef18a700f0ebc8d9938307150085d67ccfeccc0
Senyo197 <Senyo197@gmail.com>
2023-11-18 20:40:24 +0000

    list cities from California

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
---creates MySql ser user user_0d_1 and grant all privilages
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED By 'user_0d_1_pwd';
-GRANT ALL PRIVILAGES ON * . * 	TO user_0d_1@localhost;
+-- creates MySql ser user user_0d_1 and grant all privilages
+CREATE USER IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,9 @@
 -- Create the database hbtn_0d_usa if it doesn't exist
-CREATE IF NOT EXISTS DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Switch to the hbtn_0d_usa database
 USE hbtn_0d_usa;
 -- Create the states table if it doesn't exist
-CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT UNIQUE NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id));
