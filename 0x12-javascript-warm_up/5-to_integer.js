28088dd44a4d0837cf1a982e7ef3efaccf55a09f
Senyo197 <Senyo197@gmail.com>
2023-12-11 18:48:57 +0000

    convert number

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const [firstArg] = process.argv.slice(2);
+const convertNum = parseInt(firstArg);
+
+if (isNaN(firstArg)) {
+	console.log("Not a number");
+} else {
+	console.log(`My number: ${convertNum}`);
+}
