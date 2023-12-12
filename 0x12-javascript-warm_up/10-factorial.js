3f05954167c186c55038020af2e814ddb82b6656
Senyo197 <Senyo197@gmail.com>
2023-12-12 04:43:57 +0000

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const [firstArg]= process.argv.slice(2)
+const convertedNum = parseInt(firstArg)
+
+function factorial (n) {
+  if (n < 0) {
+    return -1;
+  }
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  }
+  return (n * factorial(n - 1));
+}
+
+console.log(factorial(convertedNum));
