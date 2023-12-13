95bc22f740f140170c8e7888d0210c3d7dbde31a
Senyo197 <Senyo197@gmail.com>
2023-12-13 04:11:25 +0000

    c and x

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint(c) {
+
+    const charToPrint = c || 'X';
+
+    if (this.width !== undefined && this.height !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += charToPrint;
+        }
+        console.log(row);
+      }
+    }
+  }
+}
+
+module.exports = Square;
